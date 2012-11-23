c3acb67188d2e97927dfd4af379ef8f3c923805f
chriwode <chriwode@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-23 20:40:24 +0000

    Update ext_emconf.php and refresh scheduler version in constraints
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/feusers_reminder/trunk@68022 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "feusers_reminder".
  *
- * Auto generated 23-11-2012 16:23
+ * Auto generated 23-11-2012 21:39
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -33,14 +33,14 @@ $EM_CONF[$_EXTKEY] = array(
 			'extbase' => '1.3.0',
 			'fluid' => '1.3.0',
 			'typo3' => '4.6.0-4.7.99',
-			'scheduler' => '1.2.0-1.2.99',
+			'scheduler' => '1.2.0-4.7.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"f728";s:14:"ext_tables.php";s:4:"8d8e";s:14:"ext_tables.sql";s:4:"a2da";s:21:"ExtensionBuilder.json";s:4:"3095";s:37:"Classes/Domain/Model/FrontendUser.php";s:4:"e255";s:52:"Classes/Domain/Repository/FrontendUserRepository.php";s:4:"25b4";s:31:"Classes/Tasks/ReminderTasks.php";s:4:"9a11";s:45:"Classes/Tasks/SchedulerAddFields4Reminder.php";s:4:"571d";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"9d95";s:38:"Configuration/TypoScript/constants.txt";s:4:"d41d";s:34:"Configuration/TypoScript/setup.txt";s:4:"b99a";s:40:"Resources/Private/Language/locallang.xml";s:4:"f4fc";s:54:"Resources/Private/Language/locallang_csh_scheduler.xml";s:4:"23b9";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"f781";s:50:"Resources/Private/Language/locallang_scheduler.xml";s:4:"a5bc";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"8d2d";}',
+	'_md5_values_when_last_written' => 'a:17:{s:12:"ext_icon.gif";s:4:"f728";s:14:"ext_tables.php";s:4:"8d8e";s:14:"ext_tables.sql";s:4:"a2da";s:21:"ExtensionBuilder.json";s:4:"3095";s:37:"Classes/Domain/Model/FrontendUser.php";s:4:"e255";s:52:"Classes/Domain/Repository/FrontendUserRepository.php";s:4:"25b4";s:31:"Classes/Tasks/ReminderTasks.php";s:4:"0d0b";s:45:"Classes/Tasks/SchedulerAddFields4Reminder.php";s:4:"571d";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"9d95";s:38:"Configuration/TypoScript/constants.txt";s:4:"d41d";s:34:"Configuration/TypoScript/setup.txt";s:4:"b99a";s:40:"Resources/Private/Language/locallang.xml";s:4:"f4fc";s:54:"Resources/Private/Language/locallang_csh_scheduler.xml";s:4:"23b9";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"f781";s:50:"Resources/Private/Language/locallang_scheduler.xml";s:4:"a5bc";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:14:"doc/manual.sxw";s:4:"8d2d";}',
 );
 
 ?>
